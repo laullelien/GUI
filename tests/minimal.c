5db003135cf7d7aaaeab5f442077047bee36e636
Yung Pheng THOR <yung-pheng.thor@grenoble-inp.org>
2023-05-04 16:34:02 +0200

    done clipping

--- tests/minimal.c
+++ tests/minimal.c
@@ -78,29 +78,14 @@ int main(int argc, char *argv[])
 	// Lock the surface for drawing,
 	hw_surface_lock(main_window);
 
-	ei_color_t *white = malloc(sizeof(ei_color_t));
-	white->red = 255;
-	white->green = 255;
-	white->blue = 255;
-	white->alpha = 0;
+	ei_color_t white = {255,255,255,0};
 
-	ei_color_t *black = malloc(sizeof(ei_color_t));
-	black->red = 0;
-	black->green = 0;
-	black->blue = 0;
-	black->alpha = 0;
+	ei_color_t black = {0,0,0,0};
 
-	ei_color_t *purple = malloc(sizeof(ei_color_t));
-	purple->red = 255;
-	purple->green = 0;
-	purple->blue = 255;
-	purple->alpha = 0;
+	ei_color_t purple = {255,0,255,0};
 
-	ei_color_t *yellow = malloc(sizeof(ei_color_t));
-	yellow->red = 255;
-	yellow->green = 255;
-	yellow->blue = 0;
-	yellow->alpha = 0;
+	ei_color_t yellow = {255,255,0,0};
+
 
 	ei_point_t hexa_l[9];
 	hexa_l[0].x = 100;
@@ -160,13 +145,21 @@ int main(int argc, char *argv[])
 	line_8[1].x = 500;
 	line_8[1].y = 200;
 
-	ei_fill(main_window, black, NULL);
+	ei_point_t top_left = {0, 0};
+	ei_size_t size = {400,400};
+	ei_rect_t clipper = {top_left, size};
 
-	ei_draw_polyline(main_window, hexa_l, 9, *purple, NULL);
-	ei_draw_polyline(main_window, pxl_p, 2, *purple, NULL);
-	ei_draw_polyline(main_window, hexa_h, 9, *yellow, NULL);
-	ei_draw_polyline(main_window, pxl_y, 2, *yellow, NULL);
-	ei_draw_polyline(main_window, line_8, 2, *white, NULL);
+
+	ei_fill(main_window, &black, NULL);
+	ei_fill(main_window, &yellow, &clipper);
+
+
+
+	ei_draw_polyline(main_window, hexa_l, 9, purple, NULL);
+	ei_draw_polyline(main_window, pxl_p, 2, purple, NULL);
+	ei_draw_polyline(main_window, hexa_h, 9, yellow, NULL);
+	ei_draw_polyline(main_window, pxl_y, 2, yellow, NULL);
+	ei_draw_polyline(main_window, line_8, 2, white, NULL);
 	
 	// unlock, update screen.
 	hw_surface_unlock(main_window);
@@ -179,10 +172,7 @@ int main(int argc, char *argv[])
 
 	// Free hardware resources.
 	hw_quit();
-	free(white);
-	free(black);
-	free(purple);
-	free(yellow);
+
 	// Terminate program with no error code.
 	return 0;
 }

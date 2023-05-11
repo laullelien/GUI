f5df4d1b2ee53fa9193ff716f727e3ddb242b5c0
Dorian Verney <dorian.verney@grenoble-inp.org>
2023-05-11 15:46:16 +0200

    frame done

---
+++ implem/ei_widgetclass.c
@@ -0,0 +1,22 @@
+#include "../api/ei_widgetclass.h"
+#include "ei_impl_widgetclass.h"
+#include "ei_application.h"
+#include "ei_draw_arc.h"
+
+void ei_widgetclass_register(ei_widgetclass_t *widgetclass)
+{
+    ei_frame_widgetclass_create(widgetclass);
+    // widgetclass->next=ei_button_widgetclass_create(widgetclass);
+    // widgetclass->next->next=ei_toplevel_widgetclass_create(widgetclass);
+    return;
+}
+
+ei_widgetclass_t *ei_widgetclass_from_name(ei_const_string_t name)
+{
+    ei_widgetclass_t *widgetclass = ei_app_root_widget()->wclass;
+    if (widgetclass->name[0] == 'f')
+    {
+        return widgetclass;
+    }
+    return NULL;
+}

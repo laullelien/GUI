65ad0ccdbd062d1f3ee820a72a0be6082e56ebf4
Dorian Verney <dorian.verney@grenoble-inp.org>
2023-05-11 14:48:57 +0200

    update folkoch

---
+++ implem/ei_impl_widgetclass.c
@@ -0,0 +1,140 @@
+#include "../api/ei_widgetclass.h"
+#include "ei_impl_widgetclass.h"
+#include "ei_draw.h"
+#include "ei_application.h"
+
+
+
+/* FRAME */
+ei_widget_t ei_frame_allocfunc()
+{
+    ei_widget_t frame = (ei_widget_t)malloc(sizeof(ei_impl_frame_t));
+    (*(ei_widget_t *)frame) = malloc(sizeof(ei_impl_widget_t));
+    return frame;
+}
+
+void ei_frame_releasefunc(ei_widget_t frame)
+{
+    free((ei_impl_frame_t *)frame);
+}
+
+void ei_frame_drawfunc(ei_widget_t frame,
+                       ei_surface_t surface,
+                       ei_surface_t pick_surface,
+                       ei_rect_t *clipper)
+{
+    if (((ei_impl_frame_t *)frame)->relief == ei_relief_none || ((ei_impl_frame_t *)frame)->border_width == 0)
+    {
+        ei_fill(surface, &((ei_impl_frame_t *)frame)->color, &(*(ei_widget_t *)frame)->screen_location);
+        ei_fill(pick_surface, ((ei_impl_frame_t *)frame)->ei_frame_widget->pick_color, &(*(ei_widget_t *)frame)->screen_location);
+    }
+    else if (((ei_impl_frame_t *)frame)->relief == ei_relief_raised)
+    {
+        // ei_draw_polygon(surface, );
+    }
+
+    // ei_draw_text(surface, ((ei_impl_frame_t *)frame)->color);
+    ei_widget_t children_head = ((ei_impl_frame_t *)frame)->ei_frame_widget->children_head;
+    while (children_head != NULL)
+    {
+        (*((*(ei_widget_t *)children_head)->wclass->drawfunc))(children_head, surface, pick_surface, (*(ei_widget_t *)frame)->content_rect);
+        children_head = children_head->next_sibling;
+    }
+}
+
+void ei_frame_setdefaultsfunc(ei_widget_t frame)
+{
+    // (*(ei_widget_t *)frame)->requested_size = *requested_size;
+    ((ei_impl_frame_t *)frame)->color = ei_default_background_color;
+    ((ei_impl_frame_t *)frame)->border_width = 0;
+    ((ei_impl_frame_t *)frame)->relief = ei_relief_none;
+    ((ei_impl_frame_t *)frame)->text = NULL;
+    ((ei_impl_frame_t *)frame)->text_font = ei_default_font;
+    ((ei_impl_frame_t *)frame)->text_color = ei_font_default_color;
+    ((ei_impl_frame_t *)frame)->text_anchor = ei_anc_center;
+    ((ei_impl_frame_t *)frame)->img = NULL;
+    ((ei_impl_frame_t *)frame)->img_rect = NULL;
+    ((ei_impl_frame_t *)frame)->img_anchor = ei_anc_center;
+}
+
+void ei_frame_widgetclass_create(ei_widgetclass_t *ei_frame_widgetclass)
+{
+    ei_frame_widgetclass->name[0] = 'f';
+    ei_frame_widgetclass->name[1] = 'r';
+    ei_frame_widgetclass->name[2] = 'a';
+    ei_frame_widgetclass->name[3] = 'm';
+    ei_frame_widgetclass->name[4] = 'e';
+    ei_frame_widgetclass->allocfunc = &ei_frame_allocfunc;
+    ei_frame_widgetclass->releasefunc = &ei_frame_releasefunc;
+    ei_frame_widgetclass->drawfunc = &ei_frame_drawfunc;
+    ei_frame_widgetclass->setdefaultsfunc = &ei_frame_setdefaultsfunc;
+}
+
+void ei_widgetclass_register(ei_widgetclass_t *widgetclass)
+{
+    ei_frame_widgetclass_create(widgetclass);
+    // widgetclass->next=ei_button_widgetclass_create(widgetclass);
+    // widgetclass->next->next=ei_toplevel_widgetclass_create(widgetclass);
+    return;
+}
+
+ei_widgetclass_t *ei_widgetclass_from_name(ei_const_string_t name)
+{
+    ei_widgetclass_t *widgetclass = (*(ei_widget_t *)ei_app_root_widget())->wclass;
+    if (widgetclass->name[0] == 'f')
+    {
+
+
+
+        return widgetclass;
+    }
+    return NULL;
+}
+
+
+
+
+/* BUTTON */
+
+
+
+
+
+ei_widget_t ei_button_allocfunc()
+{
+    ei_impl_widget_t* button = (ei_impl_widget_t*) malloc(sizeof(ei_button_t));
+    return button;
+}
+
+void ei_button_releasefunc(ei_widget_t button)
+{
+    free((ei_button_t *)button);
+}
+
+void ei_button_drawfunc(ei_widget_t button,
+                       ei_surface_t surface,
+                       ei_surface_t pick_surface,
+                       ei_rect_t *clipper)
+{
+    if
+}
+
+
+void ei_button_setdefaultsfunc(ei_widget button)
+{
+    (ei_impl_button_t *)button->color = ei_default_background_color;
+    (ei_impl_button_t *)button->border_width = k_default_button_border_width;
+    (ei_impl_button_t *)button->corner_radius = k_default_button_corner_radius;
+    (ei_impl_button_t *)button->relief = ei_relief_raised;
+    (ei_impl_button_t *)button->text = NULL;
+    (ei_impl_button_t *)button->text_font = ei_default_font;
+    (ei_impl_button_t *)button->text_color = ei_font_default_color;
+    (ei_impl_button_t *)button->text_anchor = ei_anc_center;
+    (ei_impl_button_t *)button->img = NULL;
+    (ei_impl_button_t *)button->img_rect = NULL;
+    (ei_impl_button_t *)button->img_anchor = ei_anc_center;
+    (ei_impl_button_t *)button->callback = NULL;
+    (ei_impl_button_t *)button->user_param = NULL;
+}
+
+
---
+++ implem/ei_impl_widgetclass.h
@@ -0,0 +1,63 @@
+#include "../api/ei_widgetclass.h"
+#include "ei_implementation.h"
+#include "ei_types.h"
+
+/* FRAME */
+
+typedef struct ei_impl_frame_t
+{
+    ei_impl_widget_t widget;
+    ei_color_t color;
+    int border_width;
+    ei_relief_t relief;
+    ei_string_t text;
+    ei_font_t text_font;
+    ei_color_t text_color;
+    ei_anchor_t text_anchor;
+    ei_surface_t img;
+    ei_rect_ptr_t img_rect;
+    ei_anchor_t img_anchor;
+} ei_frame_t;
+
+ei_widget_t ei_frame_allocfunc();
+void ei_frame_releasefunc(ei_widget_t frame);
+void ei_frame_drawfunc(ei_widget_t frame,
+                       ei_surface_t surface,
+                       ei_surface_t pick_surface,
+                       ei_rect_t *clipper);
+void ei_frame_setdefaultsfunc(ei_widget_t frame);
+void ei_frame_widgetclass_create(ei_widgetclass_t *ei_frame_widgetclass);
+void ei_widgetclass_register(ei_widgetclass_t *widgetclass);
+ei_widgetclass_t *ei_widgetclass_from_name(ei_const_string_t name);
+
+
+
+
+
+
+
+/* BUTTON */
+typedef struct ei_impl_button_t {
+    ei_impl_widget_t button_widget,
+    ei_color_t color,
+    int border_width,
+    int corner_radius,
+    ei_relief_t relief,
+    ei_string_t text,
+    ei_font_t text_font,
+    ei_color_t text_color,
+    ei_anchor_t text_anchor,
+    ei_surface_t img,
+    ei_rect_ptr_t img_rect,
+    ei_anchor_t img_anchor,
+    ei_callback_t callback,
+    ei_user_param_t user_param
+}ei_button_t;
+
+ei_widget_t ei_button_allocfunc();
+void ei_button_releasefunc(ei_widget_t button);
+void ei_button_drawfunc(ei_widget_t frame,
+                       ei_surface_t surface,
+                       ei_surface_t pick_surface,
+                       ei_rect_t *clipper);
+void ei_button_setdefaultsfunc(ei_widget button);

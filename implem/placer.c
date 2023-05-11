653ba3ece7202fedd69ad534dc25dca8336ef7d5
Dorian Verney <dorian.verney@grenoble-inp.org>
2023-05-11 17:54:25 +0200

    to be continued placer ...

---
+++ implem/placer.c
@@ -0,0 +1,17 @@
+#include "ei_placer.h"
+#include "ei_types.h"
+
+
+void		ei_place	(ei_widget_t		widget,
+				 ei_anchor_t*		anchor,
+				 int*			x,
+				 int*			y,
+				 int*			width,
+				 int*			height,
+				 float*			rel_x,
+				 float*			rel_y,
+				 float*			rel_width,
+				 float*			rel_height)
+{
+    
+}
